0b4ad49fcbcf4abdb3e483df2193747deef8a7ff
omar.sboxmx <omar.sboxmx@gmail.com>
2024-10-25 15:10:34 -0600

    * Cleanup Dockerfiles and stuff related
    * Minor improvements on the front-end

--- EPAM_Systems_Code_Test_Omar_Soto/epam_systems_code_test_omar_soto.client/src/App.tsx
+++ EPAM_Systems_Code_Test_Omar_Soto/epam_systems_code_test_omar_soto.client/src/App.tsx
@@ -6,13 +6,13 @@ function App() {
 
     return (
         <div>
-            <div className="bg-indigo-700 h-screen">
+            <div className="bg-gradient-to-r from-cyan-500 to-blue-500  min-h-screen">
                 <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
                     <div className="mx-auto max-w-2xl text-center">
                         <h2 className="text-balance text-4xl font-semibold tracking-tight text-white sm:text-5xl">
                             EPAM Systems - Code Test - Omar Soto
                         </h2>
-                        <p className="mx-auto mt-6 max-w-xl text-pretty text-lg/8 text-indigo-200">
+                        <p className="mx-auto mt-6 max-w-xl text-pretty text-lg/8 text-white">
                             Simulate a hard processing task for input data items provided by the user. 
                         </p>
                         <div className="mt-10 flex items-center justify-center gap-x-6">
